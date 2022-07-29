1afcd148ae678612d4b9bebbae84b1af7eff5529
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-07-29 13:26:05 +0300

    solution Task #3. Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+
+        int code = (int)character;
+
+        if (code >= 65 && code <= 122){
+            if (character == 'a'|| character == 'A'|| character == 'e'|| character == 'E'||
+                character == 'i'|| character == 'I'|| character == 'o'|| character == 'O'||
+                character == 'u'|| character == 'U'|| character == 'y'|| character == 'Y')
+                System.out.println("Vowel");
+            else
+                System.out.println("Consonant");
+        }
+        else
+            System.out.println("wrong alphabet!");
     }
 }
